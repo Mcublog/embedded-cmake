67ea68f57cc7a3b14376b871b0f1adfb18303006
Viacheslav <slava.k@ks2corp.com>
2022-09-03 19:27:29 +0300

    Added debug

---
+++ app/utils/app_utils.h
@@ -0,0 +1,36 @@
+
+#ifndef APP_UTILS_H
+#define APP_UTILS_H
+
+#include <stdint.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Prototypes for the standard FreeRTOS application hook (callback) functions
+ * implemented within this file.  See http://www.freertos.org/a00016.html .
+ */
+void vApplicationMallocFailedHook(void);
+void vApplicationIdleHook(void);
+void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName);
+void vApplicationTickHook(void);
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer,
+                                   StackType_t **ppxIdleTaskStackBuffer,
+                                   uint32_t *pulIdleTaskStackSize);
+void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer,
+                                    StackType_t **ppxTimerTaskStackBuffer,
+                                    uint32_t *pulTimerTaskStackSize);
+
+void traceOnEnter();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // APP_UTILS_H
