478200fec857fdb2f32db3533ed73ffa91aa4477
Viacheslav <slava.k@ks2corp.com>
2022-09-03 14:48:32 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # embedded-cmake
+
 Test Embedded Cmake Project
+
+Some testing project for Embedded purpose.
+
+[FreeRtosLib](https://github.com/Mcublog/FreeRTOS-CMake)
---
+++ main.c
@@ -0,0 +1,273 @@
+#include <pthread.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "semphr.h"
+
+#include "FreeRTOSConfig.h"
+
+/* Priorities at which the tasks are created. */
+#define mainQUEUE_RECEIVE_TASK_PRIORITY (tskIDLE_PRIORITY + 2)
+#define mainQUEUE_SEND_TASK_PRIORITY (tskIDLE_PRIORITY + 1)
+
+/* The rate at which data is sent to the queue.  The times are converted from
+milliseconds to ticks using the pdMS_TO_TICKS() macro. */
+#define mainTASK_SEND_FREQUENCY_MS pdMS_TO_TICKS(500UL)
+#define mainTIMER_SEND_FREQUENCY_MS pdMS_TO_TICKS(1000UL)
+
+/* The number of items the queue can hold at once. */
+#define mainQUEUE_LENGTH (2)
+
+/* The values sent to the queue receive task from the queue send task and the
+queue send software timer respectively. */
+#define mainVALUE_SENT_FROM_TASK (100UL)
+#define mainVALUE_SENT_FROM_TIMER (200UL)
+
+/*
+ * Prototypes for the standard FreeRTOS application hook (callback) functions
+ * implemented within this file.  See http://www.freertos.org/a00016.html .
+ */
+void vApplicationMallocFailedHook(void);
+void vApplicationIdleHook(void);
+void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName);
+void vApplicationTickHook(void);
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer,
+                                   StackType_t **ppxIdleTaskStackBuffer,
+                                   uint32_t *pulIdleTaskStackSize);
+void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer,
+                                    StackType_t **ppxTimerTaskStackBuffer,
+                                    uint32_t *pulTimerTaskStackSize);
+
+void traceOnEnter();
+
+/* When configSUPPORT_STATIC_ALLOCATION is set to 1 the application writer can
+use a callback function to optionally provide the memory required by the idle
+and timer tasks.  This is the stack that will be used by the timer task.  It is
+declared here, as a global, so it can be checked by a test that is implemented
+in a different file. */
+StackType_t uxTimerTaskStack[configTIMER_TASK_STACK_DEPTH];
+
+static SemaphoreHandle_t xStdioMutex;
+static StaticSemaphore_t xStdioMutexBuffer;
+
+static void console_init(void);
+static void console_print(const char *fmt, ...);
+
+static void DummyTask(void *pvParameters);
+
+int main(void)
+{
+    printf("Hello\r\n");
+    console_init();
+    xTaskCreate(DummyTask, "DummyTask", configMINIMAL_STACK_SIZE, NULL,
+                mainQUEUE_SEND_TASK_PRIORITY, NULL);
+    vTaskStartScheduler();
+    for (;;)
+    {
+        printf("Critacal error\r\n");
+    }
+}
+
+static void DummyTask(void *pvParameters)
+{
+    (void)pvParameters;
+    for (;;)
+    {
+        console_print("DummyTask\r\n");
+        vTaskDelay(1000);
+    }
+}
+
+static void console_init(void)
+{
+    xStdioMutex = xSemaphoreCreateMutexStatic(&xStdioMutexBuffer);
+}
+
+static void console_print(const char *fmt, ...)
+{
+    char buff[60];
+    time_t t = time(NULL);
+    strftime(buff, 60, "%T", localtime(&t));
+    va_list vargs;
+
+    va_start(vargs, fmt);
+
+    xSemaphoreTake(xStdioMutex, portMAX_DELAY);
+    printf("%s: ", buff);
+
+    vprintf(fmt, vargs);
+
+    xSemaphoreGive(xStdioMutex);
+
+    va_end(vargs);
+}
+
+void vApplicationMallocFailedHook(void) {
+    /* vApplicationMallocFailedHook() will only be called if
+    configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+    function that will get called if a call to pvPortMalloc() fails.
+    pvPortMalloc() is called internally by the kernel whenever a task, queue,
+    timer or semaphore is created.  It is also called by various parts of the
+    demo application.  If heap_1.c, heap_2.c or heap_4.c is being used, then the
+    size of the    heap available to pvPortMalloc() is defined by
+    configTOTAL_HEAP_SIZE in FreeRTOSConfig.h, and the xPortGetFreeHeapSize()
+    API function can be used to query the size of free heap space that remains
+    (although it does not provide information on how the remaining heap might be
+    fragmented).  See http://www.freertos.org/a00111.html for more
+    information. */
+    vAssertCalled(__FILE__, __LINE__);
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationIdleHook(void) {
+    /* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+    to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
+    task.  It is essential that code added to this hook function never attempts
+    to block in any way (for example, call xQueueReceive() with a block time
+    specified, or call vTaskDelay()).  If application tasks make use of the
+    vTaskDelete() API function to delete themselves then it is also important
+    that vApplicationIdleHook() is permitted to return to its calling function,
+    because it is the responsibility of the idle task to clean up memory
+    allocated by the kernel to any task that has since deleted itself. */
+
+    usleep(15000);
+    traceOnEnter();
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName) {
+    (void)pcTaskName;
+    (void)pxTask;
+
+    /* Run time stack overflow checking is performed if
+    configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+    function is called if a stack overflow is detected.  This function is
+    provided as an example only as stack overflow checking does not function
+    when running the FreeRTOS POSIX port. */
+    vAssertCalled(__FILE__, __LINE__);
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationTickHook(void) {
+    /* This function will be called by each tick interrupt if
+    configUSE_TICK_HOOK is set to 1 in FreeRTOSConfig.h.  User code can be
+    added here, but the tick hook is called from an interrupt context, so
+    code must not attempt to block, and only the interrupt safe FreeRTOS API
+    functions can be used (those that end in FromISR()). */
+}
+
+void traceOnEnter() {
+    int ret;
+    struct timeval tv = {0L, 0L};
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(0, &fds);
+    ret = select(1, &fds, NULL, NULL, &tv);
+    if (ret > 0) {
+        /* clear the buffer */
+        char buffer[200];
+        read(1, &buffer, 200);
+    }
+}
+
+void vLoggingPrintf(const char *pcFormat, ...) {
+    va_list arg;
+
+    va_start(arg, pcFormat);
+    vprintf(pcFormat, arg);
+    va_end(arg);
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationDaemonTaskStartupHook(void) {
+    /* This function will be called once only, when the daemon task starts to
+    execute    (sometimes called the timer task).  This is useful if the
+    application includes initialisation code that would benefit from executing
+    after the scheduler has been started. */
+}
+/*-----------------------------------------------------------*/
+
+void vAssertCalled(const char *const pcFileName, unsigned long ulLine) {
+    static BaseType_t xPrinted = pdFALSE;
+    volatile uint32_t ulSetToNonZeroInDebuggerToContinue = 0;
+
+    /* Called if an assertion passed to configASSERT() fails.  See
+    http://www.freertos.org/a00110.html#configASSERT for more information. */
+
+    /* Parameters are not used. */
+    (void)ulLine;
+    (void)pcFileName;
+
+    taskENTER_CRITICAL();
+    {
+        /* Stop the trace recording. */
+        if (xPrinted == pdFALSE) {
+            xPrinted = pdTRUE;
+        }
+
+        /* You can step out of this function to debug the assertion by using
+        the debugger to set ulSetToNonZeroInDebuggerToContinue to a non-zero
+        value. */
+        while (ulSetToNonZeroInDebuggerToContinue == 0) {
+            __asm volatile("NOP");
+            __asm volatile("NOP");
+        }
+    }
+    taskEXIT_CRITICAL();
+}
+/*-----------------------------------------------------------*/
+
+/* configUSE_STATIC_ALLOCATION is set to 1, so the application must provide an
+implementation of vApplicationGetIdleTaskMemory() to provide the memory that is
+used by the Idle task. */
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer,
+                                   StackType_t **ppxIdleTaskStackBuffer,
+                                   uint32_t *pulIdleTaskStackSize) {
+    /* If the buffers to be provided to the Idle task are declared inside this
+    function then they must be declared static - otherwise they will be
+    allocated on the stack and so not exists after this function exits. */
+    static StaticTask_t xIdleTaskTCB;
+    static StackType_t uxIdleTaskStack[configMINIMAL_STACK_SIZE];
+
+    /* Pass out a pointer to the StaticTask_t structure in which the Idle task's
+    state will be stored. */
+    *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
+
+    /* Pass out the array that will be used as the Idle task's stack. */
+    *ppxIdleTaskStackBuffer = uxIdleTaskStack;
+
+    /* Pass out the size of the array pointed to by *ppxIdleTaskStackBuffer.
+    Note that, as the array is necessarily of type StackType_t,
+    configMINIMAL_STACK_SIZE is specified in words, not bytes. */
+    *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+}
+/*-----------------------------------------------------------*/
+
+/* configUSE_STATIC_ALLOCATION and configUSE_TIMERS are both set to 1, so the
+application must provide an implementation of vApplicationGetTimerTaskMemory()
+to provide the memory that is used by the Timer service task. */
+void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer,
+                                    StackType_t **ppxTimerTaskStackBuffer,
+                                    uint32_t *pulTimerTaskStackSize) {
+    /* If the buffers to be provided to the Timer task are declared inside this
+    function then they must be declared static - otherwise they will be
+    allocated on the stack and so not exists after this function exits. */
+    static StaticTask_t xTimerTaskTCB;
+
+    /* Pass out a pointer to the StaticTask_t structure in which the Timer
+    task's state will be stored. */
+    *ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
+
+    /* Pass out the array that will be used as the Timer task's stack. */
+    *ppxTimerTaskStackBuffer = uxTimerTaskStack;
+
+    /* Pass out the size of the array pointed to by *ppxTimerTaskStackBuffer.
+    Note that, as the array is necessarily of type StackType_t,
+    configMINIMAL_STACK_SIZE is specified in words, not bytes. */
+    *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
+}
