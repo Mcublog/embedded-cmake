0ad1706f8b627c09d43f1256bfbb05aa073baa69
Viacheslav <slava.k@ks2corp.com>
2022-09-04 14:04:12 +0300

    Cleanup

--- main.c
+++ main.c
@@ -34,33 +34,17 @@
 //<<----------------------
 
 /* Priorities at which the tasks are created. */
-#define mainQUEUE_RECEIVE_TASK_PRIORITY (tskIDLE_PRIORITY + 2)
 #define mainQUEUE_SEND_TASK_PRIORITY (tskIDLE_PRIORITY + 1)
-
-/* The rate at which data is sent to the queue.  The times are converted from
-milliseconds to ticks using the pdMS_TO_TICKS() macro. */
-#define mainTASK_SEND_FREQUENCY_MS pdMS_TO_TICKS(500UL)
-#define mainTIMER_SEND_FREQUENCY_MS pdMS_TO_TICKS(1000UL)
-
-/* The values sent to the queue receive task from the queue send task and the
-queue send software timer respectively. */
-#define mainVALUE_SENT_FROM_TASK (100UL)
-#define mainVALUE_SENT_FROM_TIMER (200UL)
-
-
-static SemaphoreHandle_t xStdioMutex;
-static StaticSemaphore_t xStdioMutexBuffer;
-
-// static void console_init(void);
-// static void console_print(const char *fmt, ...);
 
 static void DummyTask(void *pvParameters);
 
 int main(void)
 {
     LOG_INFO("Hello");
-    // console_init();
-    xTaskCreate(DummyTask, "DummyTask", configMINIMAL_STACK_SIZE, NULL,
+
+    static char *name = "DummyTask0";
+
+    xTaskCreate(DummyTask, name, configMINIMAL_STACK_SIZE, name,
                 mainQUEUE_SEND_TASK_PRIORITY, NULL);
     vTaskStartScheduler();
     for (;;)
@@ -71,34 +55,10 @@ int main(void)
 
 static void DummyTask(void *pvParameters)
 {
-    (void)pvParameters;
+    const char *name = (const char*)pvParameters;
     for (;;)
     {
-        LOG_INFO("DummyTask");
+        LOG_INFO("Hello from %s", name);
         vTaskDelay(1000);
     }
 }
-
-// static void console_init(void)
-// {
-//     xStdioMutex = xSemaphoreCreateMutexStatic(&xStdioMutexBuffer);
-// }
-
-// static void console_print(const char *fmt, ...)
-// {
-//     char buff[60];
-//     time_t t = time(NULL);
-//     strftime(buff, 60, "%T", localtime(&t));
-//     va_list vargs;
-
-//     va_start(vargs, fmt);
-
-//     xSemaphoreTake(xStdioMutex, portMAX_DELAY);
-//     printf("%s: ", buff);
-
-//     vprintf(fmt, vargs);
-
-//     xSemaphoreGive(xStdioMutex);
-
-//     va_end(vargs);
-// }
